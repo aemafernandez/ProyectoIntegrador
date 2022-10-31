9fc6c0110455407c1fd0afbb124af41fb1676377
aemafernandez <111377730+aemafernandez@users.noreply.github.com>
2022-10-31 19:13:24 -0300

    Add files via upload

---
+++ ProyectoIntegrador/BackEnd/src/main/EmfeApplication.java
@@ -0,0 +1,13 @@
+package com.portfemfe.Emfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmfeApplication.class, args);
+	}
+
+}
